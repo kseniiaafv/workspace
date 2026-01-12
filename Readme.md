e59fed8c4136b76aeb153b7047b5432fc3154bdb
Maria <kitina.mariia@lit1533.ru>
2026-01-12 19:04:35 +0300

    Обновили инструкцию + русифицировали джанго, добавили статики и медиа

--- Readme.md
+++ Readme.md
@@ -3,4 +3,21 @@
 ## Активация виртуального окружения
 ```bash
 source .venv/bin/activate
+.\.venv\Scripts\Activate.ps1
+```
+
+Когда мы меняем models.py
+```bash
+python manage.py makemigrations
+python manage.py migrate
+```
+
+Создание суперпользователя
+```bash
+python manage.py createsuperuser
+```
+
+Запуск сервера
+```bash
+python manage.py runserver
 ```
--- config/settings.py
+++ config/settings.py
@@ -95,7 +95,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
@@ -108,3 +108,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / "media"
