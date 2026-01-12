8075f15c349144d549a7678b3a5fcab1bc853d62
Maria <kitina.mariia@lit1533.ru>
2026-01-12 19:06:47 +0300

    Создали модель настроек сайта, добавили ее в админку, обновили базу данных + подключили стили в urls

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import (
+    SiteSettings
+)
 
-# Register your models here.
+
+@admin.register(SiteSettings)
+class SiteSettingsAdmin(admin.ModelAdmin):
+    list_display = ("site_name", "updated_at")
+
--- core/models.py
+++ core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class SiteSettings(models.Model):
+    site_name = models.CharField("Название сайта", max_length=120, default="KANISDOG")
+
+    hero_text = models.TextField("Текст (Hero)", blank=True)
+    hero_image = models.ImageField("Картинка (Hero)", upload_to="hero/", blank=True, null=True)
+
+    updated_at = models.DateTimeField("Обновлено", auto_now=True)
+
+    class Meta:
+        verbose_name = "Настройки сайта"
+        verbose_name_plural = "Настройки сайта"
