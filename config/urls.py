2465b80eb5ab536686a3f0658af3274f2d357442
Maria <kitina.mariia@lit1533.ru>
2026-01-18 19:13:41 +0300

    Основа для верстки

--- config/urls.py
+++ config/urls.py
@@ -3,8 +3,11 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
+from core.views import home
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", home, name="home"),
 ]
 
 if settings.DEBUG:
